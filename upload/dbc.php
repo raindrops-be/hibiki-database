3615d66958ec1bbeca61732fcc33282575e93033
raindrops-be <151705752+raindrops-be@users.noreply.github.com>
2023-11-22 13:53:01 +0900

    Create dbc.php

---
+++ upload/dbc.php
@@ -0,0 +1,65 @@
+<?php
+
+function dbc()
+{
+  $host = "localhost";
+  $dbname = "file_db";
+  $user = "root";
+  $pass = "root";
+
+  $dns = "mysql:host=$host;dbname=$dbname;charset=utf8";
+
+  try { 
+    $pdo = new PDO($dns, $user, $pass, [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+    return $pdo;
+  } catch (PDOException $e) { 
+    exit($e->getMessage());
+  }
+}
+
+/**
+ * ファイルデータを保存
+ * @param string $filename ファイル名
+ * @param string $save_path 保存先のパス
+ * @param string $caption 投稿の説明
+ * @return bool $result
+ */
+function fileSave($filename, $save_path, $caption )
+{
+    $result = false;
+
+    $sql = "INSERT INTO file_table (file_name, file_path, description) VALUES(?, ?, ?)";
+    try{
+        $stmt = dbc()->prepare($sql);
+        $stmt->bindValue(1, $filename);
+        $stmt->bindValue(2, $save_path);
+        $stmt->bindValue(3, $caption);
+        $result = $stmt->execute();
+        return $result;
+    }catch(\Exception $e){
+     echo ($e->getMessage());
+     return $result;
+    }
+
+}
+
+/**
+ * ファイルデータを取得
+
+ * @return arrat $filedata
+ */
+function gatAllfile()
+{
+  $sql = "SELECT * FROM file_table";
+
+  $fileData = dbc()->query($sql);
+
+  return $fileData;
+}
+
+function h($s) {
+  return htmlspecialchars($s, ENTQUOTES, "UTF-8");
+}
